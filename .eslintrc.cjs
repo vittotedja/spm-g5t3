7b6620d6f45cdc8bf71507216c9fd9e8067b6903
VITTO SURYA TEDJA <vittosuryat.2021@scis.smu.edu.sg>
2023-09-12 17:51:52 +0800

    init repo

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,44 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	root: true,
+	env: {browser: true, es2020: true, jest: true},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended-type-checked',
+		'plugin:@typescript-eslint/stylistic-type-checked',
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:react/jsx-runtime',
+		'plugin:prettier/recommended',
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	plugins: ['react-refresh', 'react', '@typescript-eslint'],
+	rules: {
+		'react-refresh/only-export-components': [
+			'warn',
+			{allowConstantExport: true},
+		],
+		'@typescript-eslint/explicit-function-return-type': 0,
+		'@typescript-eslint/explicit-module-boundary-types': 0,
+		'react/react-in-jsx-scope': 0,
+		'import/no-anonymous-default-export': 'off',
+		'no-tabs': 0,
+		'max-len': [
+			'error',
+			{
+				code: 100,
+				ignoreComments: true, //"comments": 80
+				ignoreUrls: true,
+				ignoreStrings: true,
+				ignoreTemplateLiterals: true,
+			},
+		],
+		'linebreak-style': 'off',
+	},
+	settings: {
+		react: {
+			pragma: 'React',
+			version: 'detect',
+		},
+	},
+};
