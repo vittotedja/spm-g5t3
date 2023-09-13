eb2fa08c6feb2e65b68312b233f29b2351560ddf
VITTO SURYA TEDJA <vittosuryat.2021@scis.smu.edu.sg>
2023-09-13 19:52:55 +0800

    react router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import './App.css';
+import {Route, Routes} from 'react-router-dom';
 
 function App() {
 	return (
 		<>
-			<div>
-				<h1 className="text-3xl font-bold underline">SPM G5T3</h1>
-				<p className="text-red-300">testing an issue</p>
-			</div>
+			<Routes>
+				<Route path="/" element={<h1>Home</h1>} />
+				<Route path="/login" element={<h1>Login</h1>} />
+				<Route
+					path="*"
+					element={
+						<h1 className="text-3xl text-center text-red-500">
+							404 Not Found
+						</h1>
+					}
+				/>
+			</Routes>
 		</>
 	);
 }
