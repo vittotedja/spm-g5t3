1dcd5e6f7ac92b6554e00f95efeca7dcc6838037
yozafard <yozafardhs.2021@scis.smu.edu.sg>
2023-09-19 23:01:48 +0800

    front-end for login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,6 +4,7 @@ import { createClient } from '@supabase/supabase-js';
 import { useRef, useState } from 'react'
 import { useAuth } from '../components/Auth';
 // import { useHistory } from 'react-router-dom';
+import react from '../assets/react.svg';
 
 const supabaseUrl = 'https://wbsagjngbxrrzfktkvtt.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indic2Fnam5nYnhycnpma3RrdnR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ2NjU0MjcsImV4cCI6MjAxMDI0MTQyN30.X_EkPcpKarJkJk3FYExVrPE3Y73CvOzkP6Yhp0oyC0A'
@@ -41,18 +42,32 @@ export function Login() {
   }
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="input-email">Email</label>
-        <input id="input-email" type="email" ref={emailRef} />
+    <div className="flex h-screen w-screen rounded-lg p-0">
+      <div className="flex flex-col justify-center items-start w-1/2 bg-emerald-600 text-white p-12">
+        {/* Replace this with your logo */}
+        <div className="text-3xl mb-8">Welcome to</div>
+        <div className="text-6xl font-bold mb-8">GlassWindow</div>
+        <div className="text-3xl mb-8">One stop internal hiring platform</div>
+      </div>
+      
+      <div className="flex flex-col justify-center items-start w-1/2 bg-white p-12">
+      <div className="text-5xl font-bold mb-6">Login</div>
+        <form onSubmit={handleSubmit} className="space-y-4 w-full">
+          <div className='justify-start items-start text-lg font-medium text-gray-600 text-left'>
+            <label htmlFor="signup-email" className="block mb-2"> Email </label>
+            <input id="signup-email" type="email" ref={emailRef} className="p-2 w-full border rounded-md bg-gray-200" />
+          </div>
 
-        <label htmlFor="input-password">Password</label>
-        <input id="input-password" type="password" ref={passwordRef} />
+          <div className='justify-start items-start text-lg font-medium text-gray-600 text-left'>
+            <label htmlFor="signup-password" className="block mb-2">Password</label>
+            <input id="signup-password" type="password" ref={passwordRef} className="p-2 w-full border rounded-md bg-gray-200" />
+          </div>
 
-        <br />
-
-        <button type="submit">Login</button>
-      </form>
-    </>
-  )
+          <button type="submit" className="font-bold mt-6 bg-emerald-600 text-white p-2 w-1/4 rounded-md hover:bg-emerald-500 focus:outline-none focus:border-emerald-700 focus:ring focus:ring-emerald-200 justify-content-end float-right">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
