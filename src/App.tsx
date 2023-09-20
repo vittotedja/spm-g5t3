1e1036abd3d81cbeaeed277ea4bb9188a1b75c25
VITTO SURYA TEDJA <vittosuryat.2021@scis.smu.edu.sg>
2023-09-20 13:17:50 +0800

    feat: view posted roles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import {Route, Routes} from 'react-router-dom';
+import ManagerRoleDetails from './pages/ManagerRoleDetails';
 
 function App() {
 	return (
@@ -7,6 +8,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<h1>Home</h1>} />
 				<Route path="/login" element={<h1>Login</h1>} />
+				<Route path="/manager" element={<ManagerRoleDetails />} />
 				<Route
 					path="*"
 					element={
