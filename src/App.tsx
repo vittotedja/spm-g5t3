9dfaf9096e592668600ad40647d7abd6eb905e88
Jordian Renaldi <jrenaldi.2021@scis.smu.edu.sg>
2023-09-20 10:36:08 +0800

    Changes to imports and added filters

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import './App.css';
 import {Route, Routes} from 'react-router-dom';
-import TestPage from './pages/TestPage';
 import RoleDetails from './pages/RoleDetails';
+import RoleListing from './pages/RoleListing';
 
 function App() {
 	return (
 		<>
 			<Routes>
-				<Route path="/" Component={TestPage} />
+				<Route path="/" element={<h1>Hello</h1>} />
 				<Route path="/login" element={<h1>Login</h1>} />
-				<Route path="/role-details/:role_ID" Component={RoleDetails} />
+				<Route path="/role-listing" Component={RoleListing} />
+				<Route path="/role-listing/:role_ID" Component={RoleDetails} />
 				<Route
 					path="*"
 					element={
