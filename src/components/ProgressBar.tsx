13b321c7e0e6ba6b0890dad7162ec3eb8f72562d
Jordian Renaldi <jrenaldi.2021@scis.smu.edu.sg>
2023-09-21 15:59:21 +0800

    Changes to ensure pill has max and min values

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,29 +1,68 @@
-// SimpleProgressBar.tsx
-import React from 'react';
+import React from "react";
 
 interface ProgressBarProps {
   percentage: number;
 }
 
 const ProgressBar: React.FC<ProgressBarProps> = ({ percentage }) => {
-    let colorClass;
-  
-    if (percentage > 70) {
-      colorClass = '#299B71'; // Hex color for green
-    } else if (percentage > 50) {
-      colorClass = '#FFCA0F'; // Hex color for yellow
-    } else {
-      colorClass = '#AD0626'; // Hex color for red
-    }
+  let colorClass;
 
-    return (
-      <div className="w-full bg-gray-200 rounded-full">
-        <div className={`text-s text-blue-100 text-center p-0.5 leading-none rounded-full h-5.5`} style={{ width: `${percentage}%`, background: colorClass }}>
-          {percentage}%
-        </div>
+  if (percentage > 66) {
+    colorClass = "#299B71"; // Hex color for green
+  } else if (percentage > 33) {
+    colorClass = "#FFCA0F"; // Hex color for yellow
+  } else {
+    colorClass = "#AD0626"; // Hex color for red
+  }
+
+  var percentageString;
+  if (percentage > 66) {
+    percentageString = (
+      <div
+        className={`text-s text-white text-center p-0.5 leading-none rounded-full h-5.5`}
+        style={{ width: `${percentage}%`, background: colorClass }}
+      >
+        {percentage}%
       </div>
     );
-  };
-  
+  } else if (percentage > 33) {
+    percentageString = (
+      <div
+        className={`text-s text-white text-center p-0.5 leading-none rounded-full h-5.5`}
+        style={{ width: `${percentage}%`, background: colorClass }}
+      >
+        {percentage}%
+      </div>
+    );
+  } else if (percentage == 0) {
+    percentageString = (
+      <div
+        className={`text-s text-black text-center p-0.5 leading-none rounded-full h-5.5 flex items-center justify-center w-full`}
+      >
+        {percentage}%
+      </div>
+    );
+  } else {
+    percentageString = (
+      <>
+        <div
+          className={`text-s text-white text-center p-0.5 leading-none rounded-full h-5.5`}
+          style={{ width: `${percentage}%`, background: colorClass }}
+        >
+          <span style={{ visibility: "hidden" }}>0</span>
+        </div>
+        <div className="absolute inset-0 flex items-center justify-center">
+          {percentage}%
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <div className="w-full bg-gray-200 rounded-full relative font-bold">
+      {percentageString}
+    </div>
+  );
+};
 
 export default ProgressBar;
