c40f0c3fcdc22ecfd6997fe1dd0c3605726c9a2b
Jordian Renaldi <jrenaldi.2021@scis.smu.edu.sg>
2023-09-22 22:50:21 +0800

    Turn off hover state if loading

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -20,16 +20,22 @@ const Button: FC<ButtonProps> = ({
     "relative flex items-center justify-center text-transform-none rounded-lg px-4 py-2 text-lg h-10";
 
   const stylesMap = {
-    normal: "bg-olive-green hover:bg-olive-green-dark text-white",
-    green: "bg-green hover:bg-green-dark text-white",
-    red: "bg-red hover:bg-red-dark text-white",
-    disabled: "bg-gray-400 hover:bg-gray-500 cursor-not-allowed",
+    normal: loading
+      ? "bg-olive-green text-white"
+      : "bg-olive-green hover:bg-olive-green-dark text-white",
+    green: loading
+      ? "bg-green text-white"
+      : "bg-green hover:bg-green-dark text-white",
+    red: loading ? "bg-red text-white" : "bg-red hover:bg-red-dark text-white",
+    disabled: "bg-gray-400 cursor-not-allowed",
   };
 
   return (
     <button
       onClick={onClick}
-      className={`${baseStyles} ${stylesMap[styleType]} ${className}`}
+      className={`${baseStyles} ${stylesMap[styleType]} ${className} ${
+        loading ? "pointer-events-none" : ""
+      }`}
       disabled={styleType === "disabled" || loading}
     >
       <span className={loading ? "opacity-0" : "opacity-100"}>{children}</span>
