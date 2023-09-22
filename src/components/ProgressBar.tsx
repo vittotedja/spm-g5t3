35e87433f425fc769fdbe5943856ba4ddaebc429
Jordian Renaldi <jrenaldi.2021@scis.smu.edu.sg>
2023-09-22 22:41:24 +0800

    Updated UI of percentage bar

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -6,61 +6,29 @@ interface ProgressBarProps {
 
 const ProgressBar: React.FC<ProgressBarProps> = ({ percentage }) => {
   let colorClass;
+  const validPercentage = Math.min(Math.max(percentage, 0), 100);
 
-  if (percentage > 66) {
+  if (validPercentage > 66) {
     colorClass = "#299B71"; // Hex color for green
-  } else if (percentage > 33) {
+  } else if (validPercentage > 33) {
     colorClass = "#FFCA0F"; // Hex color for yellow
+  } else if (validPercentage == 0) {
+    colorClass = "bg-gray-200";
   } else {
     colorClass = "#AD0626"; // Hex color for red
   }
 
-  var percentageString;
-  if (percentage > 66) {
-    percentageString = (
-      <div
-        className={`text-s text-white text-center p-0.5 leading-none rounded-full h-5.5`}
-        style={{ width: `${percentage}%`, background: colorClass }}
-      >
-        {percentage}%
-      </div>
-    );
-  } else if (percentage > 33) {
-    percentageString = (
-      <div
-        className={`text-s text-white text-center p-0.5 leading-none rounded-full h-5.5`}
-        style={{ width: `${percentage}%`, background: colorClass }}
-      >
-        {percentage}%
-      </div>
-    );
-  } else if (percentage == 0) {
-    percentageString = (
-      <div
-        className={`text-s text-black text-center p-0.5 leading-none rounded-full h-5.5 flex items-center justify-center w-full`}
-      >
-        {percentage}%
-      </div>
-    );
-  } else {
-    percentageString = (
-      <>
-        <div
-          className={`text-s text-white text-center p-0.5 leading-none rounded-full h-5.5`}
-          style={{ width: `${percentage}%`, background: colorClass }}
-        >
-          <span style={{ visibility: "hidden" }}>0</span>
-        </div>
-        <div className="absolute inset-0 flex items-center justify-center">
-          {percentage}%
-        </div>
-      </>
-    );
-  }
-
   return (
-    <div className="w-full bg-gray-200 rounded-full relative font-bold">
-      {percentageString}
+    <div className="w-full bg-gray-200 rounded-full relative font-bold overflow-hidden">
+      <div
+        className={`text-s text-white text-center p-0.5 leading-none rounded-full h-5.5`}
+        style={{ width: `${validPercentage}%`, background: colorClass }}
+      >
+        <span style={{ visibility: "hidden" }}>0</span>
+      </div>
+      <div className="absolute inset-0 flex items-center justify-center">
+        {validPercentage}%
+      </div>
     </div>
   );
 };
