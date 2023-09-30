1958d13f1fac87266de21118996a5195068d1445
Vincent Lewi <vincentlewii@gmail.com>
2023-09-30 17:29:55 +0800

    trying to fix unexpected token '<'

--- src/utilities/Services.tsx
+++ src/utilities/Services.tsx
@@ -1,5 +1,3 @@
-const api_path = window.location.host === 'localhost:5173' ? 'http://localhost:8000/' : window.location.host + '/'
-
 export async function setInitial(setResult: Function, url: string, list: boolean = true) {
     const response = await getAsync(url)
     const data = await response.json()
@@ -7,12 +5,14 @@ export async function setInitial(setResult: Function, url: string, list: boolean
 }
 
 export const getAsync = (url: string, token: null | string = null) => {
-    const headers = token ? { Authorization: `Bearer ${token}` } : undefined
-    return fetch(api_path + url, { headers })
+    const headers = token ? { Authorization: `Bearer ${token}` } : undefined;
+    const api_path = import.meta.env.NODE_ENV === 'production' ? window.location.host + '/' : 'http://localhost:8000/'
+    return fetch(api_path + url, { headers });
 };
 
 export const postAsync = (url: string, data: any, token: null | string = null) => {
-    const headers = token ? { Authorization: `Bearer ${token}` } : undefined
+    const headers = token ? { Authorization: `Bearer ${token}` } : undefined;
+    const api_path = import.meta.env.NODE_ENV === 'production' ? window.location.host + '/' : 'http://localhost:8000/'
     return fetch(api_path + url, {
         method: "POST",
         headers: {
@@ -20,12 +20,13 @@ export const postAsync = (url: string, data: any, token: null | string = null) =
             ...headers,
         },
         body: JSON.stringify(data),
-    })
-}
+    });
+};
 
 export const putAsync = (url: string, data: any, token: null | string = null) => {
-    const headers = token ? { Authorization: `Bearer ${token}` } : undefined
-    console.log(JSON.stringify(data))
+    const headers = token ? { Authorization: `Bearer ${token}` } : undefined;
+    const api_path = import.meta.env.NODE_ENV === 'production' ? window.location.host + '/' : 'http://localhost:8000/'
+    console.log(data)
     return fetch(api_path + url, {
         method: "PUT",
         headers: {
@@ -33,5 +34,6 @@ export const putAsync = (url: string, data: any, token: null | string = null) =>
             ...headers,
         },
         body: JSON.stringify(data),
-    })
-}
+        
+    });
+};
