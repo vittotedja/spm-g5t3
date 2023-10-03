0b9b749ad970a24cbfc201d8b32373fd63923350
DennisH18 <111347933+DennisH18@users.noreply.github.com>
2023-10-03 15:03:58 +0800

    com

---
+++ api/get_role_details.py
@@ -0,0 +1,38 @@
+import math
+from fastapi import FastAPI, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+url: str = os.getenv("SUPABASE_URL")
+key: str = os.getenv("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+app = FastAPI(swagger_ui_parameters={"displayRequestDuration": True})
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+router = APIRouter()
+
+#view a single role details
+@app.get("/api/role")
+@router.get("/api/role")
+async def get_role(roleid: int = None):
+    if roleid:
+        role_response = supabase.from_("role").select("*").eq("role_id", str(roleid)).execute()
+        return {"data": role_response}
+
+    
---
+++ api/post_apply.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI, APIRouter, Body
+from fastapi.middleware.cors import CORSMiddleware
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+from pydantic import BaseModel, Field
+
+# Load environment variables from .env file
+load_dotenv()
+
+url: str = os.getenv("SUPABASE_URL")
+key: str = os.getenv("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+app = FastAPI(swagger_ui_parameters={"displayRequestDuration": True})
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+router = APIRouter()
+
+from datetime import datetime, timezone
+
+class TimestampzConverter(BaseModel):
+    rootmodel: datetime
+    def __get__(self, obj, type=None):
+        return super().__get__(obj, type).isoformat()
+
+class Application(BaseModel):
+    application_id: int
+    staff_id: int
+    role_id: int
+    status: str = "Pending"
+    statement: str
+
+@app.post("/api/apply")
+@router.post("/api/apply")
+async def post_apply(application: Application = Body(...)):
+    
+    try:
+        data, error = supabase.table('application').insert([
+            application.dict()
+        ]).execute()
+
+        print(application.application_id)
+
+        if error:
+            print(error)  # Log the error for debugging
+            return {"success": False, "error": error}
+        else:
+            return {"success": True, "data": data}  # Return the first item in the response
+
+    
+    except Exception as e:
+        return {"success": False, "error": e}
