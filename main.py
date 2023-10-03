7259bca1a103d6b938d5e23de7c27beb68efb204
DennisH18 <111347933+DennisH18@users.noreply.github.com>
2023-10-03 11:57:37 +0800

    commiteed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from api import get_staff, get_role, get_staff_skill, get_role_skill, get_staff_role_skill, create_role_listing, get_staff_role
+from api import get_staff, get_role, get_staff_skill, get_role_skill, get_staff_role_skill, role, skillmatch, applicants, apply, totalapplications, create_role_listing, get_staff_role
 
 app = FastAPI()
 app.add_middleware(
@@ -16,5 +16,10 @@ app.include_router(get_role.router)
 app.include_router(get_staff_skill.router)
 app.include_router(get_role_skill.router)
 app.include_router(get_staff_role_skill.router)
+app.include_router(role.router)
+app.include_router(skillmatch.router)
+app.include_router(applicants.router)
+app.include_router(apply.router)
+app.include_router(totalapplications.router)
 app.include_router(create_role_listing)
 app.include_router(get_staff_role.router)
