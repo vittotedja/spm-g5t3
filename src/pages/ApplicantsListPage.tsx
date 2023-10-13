699b1eb5a35440b08dad5fb6c3c7aa0be69a33bf
DennisH18 <111347933+DennisH18@users.noreply.github.com>
2023-10-13 19:00:51 +0800

    edited ApplicantsListPage

--- src/pages/ApplicantsListPage.tsx
+++ src/pages/ApplicantsListPage.tsx
@@ -1,74 +1,80 @@
-import ProgressBar from '../components/ProgressBar';
-import {useParams} from 'react-router-dom';
-import {useEffect, useState} from 'react';
-import {AiOutlineArrowLeft} from 'react-icons/ai';
-import {FaLocationDot} from 'react-icons/fa6';
-import Button from '../components/Button';
-import {setInitial} from '../utilities/Services';
-import formatDate from '../utilities/Utiliities';
-import {useNavigate} from 'react-router-dom';
-
-interface Role {
-	role_id: number;
-	role_name: string;
-	created_at: Date;
-	appl_close_date: Date;
-	dept: string;
-	level: string;
-	location: string;
-}
+import ProgressBar from "../components/ProgressBar";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import { FaLocationDot } from "react-icons/fa6";
+import Button from "../components/Button";
+import { setInitial } from "../utilities/Services";
+import formatDate from "../utilities/Utiliities";
+import { useNavigate } from "react-router-dom";
 
 const ApplicantsListPage = () => {
   const navigate = useNavigate();
-  const role_ID = useParams<{ role_id: string | undefined }>();
-  const [roleData, setRoleData] = useState<Role>(Object);
-  const role_id = role_ID.role_id;
+  const param = useParams<{ listing_id: string }>();
+  const [listingData, setListingData] = useState<any>(Object);
   const [applicantsData, setApplicantsData] = useState<any>(Object);
   const [view, setView] = useState<string>("Applied");
-  //TO DO: get listing_id from role listing page
+
+  let listing_id: number;
+
+  if (param.listing_id) {
+    if (/^\d+$/.test(param.listing_id)) {
+      listing_id = parseInt(param.listing_id);
+    } else {
+      return (
+        <div>Error 404 There is no Listing with id {param.listing_id}</div>
+      );
+    }
+  } else {
+    return <div>Error 404</div>;
+  }
+
   useEffect(() => {
     async function fetchData() {
-        setInitial(setRoleData, `api/role?role_id=${role_id}`,false);
-        setInitial(setApplicantsData, `api/application?role_id=${role_id}`)
+      setInitial(setListingData, `api/listing?listing_id=${listing_id}`, false);
+      setInitial(setApplicantsData, `api/application?role_id=${listing_id}`);
     }
     fetchData();
   }, []);
 
+  const role = listingData.role;
 
+  const roleName = role ? role.role_name : null;
+  const roleDept = role ? role.role_department : null;
 
-	if (!applicantsData) {
-		return <div>Loading...</div>;
-	}
+  if (!applicantsData) {
+    return <div>Loading...</div>;
+  }
 
-  if (!roleData) {
+  if (!listingData) {
     return (
-      <div className="text-3xl">
-        Error 404 There is no Role with the ID {role_id}
-      </div>
+      <div className="text-3xl">Error 404 There is no Role with the ID</div>
     );
   }
 
-	let totalRows = 0;
+  let totalRows = 0;
 
-	for (const key in applicantsData) {
-		if (applicantsData.hasOwnProperty(key)) {
-			totalRows++;
-		}
-	}
+  for (const key in applicantsData) {
+    if (applicantsData.hasOwnProperty(key)) {
+      totalRows++;
+    }
+  }
 
-	var close_date = formatDate(
-		roleData.appl_close_date ? new Date(roleData.appl_close_date) : null
-	);
-	var created_at = formatDate(
-		roleData.created_at ? new Date(roleData.created_at) : null
-	);
+  var close_date = formatDate(
+    listingData.application_close_date
+      ? new Date(listingData.application_close_date)
+      : null
+  );
+  var creation_date = formatDate(
+    listingData.creation_date ? new Date(listingData.creation_date) : null
+  );
 
-	const selectShortlist = async () => {
-		setView('Shortlisted');
-	};
-	const selectApplicants = async () => {
-		setView('Applied');
-	};
+  const selectShortlist = async () => {
+    setView("Shortlisted");
+  };
+  const selectApplicants = async () => {
+    setView("Applied");
+  };
 
   return (
     <div className="container mx-auto mt-6">
@@ -83,7 +89,7 @@ const ApplicantsListPage = () => {
         <Button
           styleType="green"
           className="mr-10"
-          onClick={() => navigate(`/update-role/${role_id}`)}
+          onClick={() => console.log("Update Role")}
         >
           Update Role
         </Button>
@@ -95,34 +101,32 @@ const ApplicantsListPage = () => {
               <div className="flex flex-col lg:flex-row">
                 <div className="w-full lg:pr-96 lg:mr-60">
                   <h2 className="text-3xl font-bold text-gray-800 text-left mb-3 whitespace-normal max-h-16">
-                    {roleData.role_name}
+                    {roleName}
                   </h2>
                   <p className="text-l text-gray-600 text-left mb-1 whitespace-nowrap">
-                    {created_at}
+                    {creation_date}
                   </p>
                   <div className="flex items-center">
                     <FaLocationDot className="text-gray-400" />
                     <p className="text-l text-emerald-900 italic text-left ml-2">
-                      {roleData.location}
+                      {listingData.listing_location}
                     </p>
                   </div>
                 </div>
               </div>
-              <div className="w-full lg:w-1/3 p-4 mb-3 lg:mb-0">
+              <div className="w-full p-4 mb-3 lg:mb-0">
                 <h3 className="text-xl font-bold text-gray-800 mb-4 text-left">
                   No of Applicants
                 </h3>
                 <p className="text-l text-gray-600 text-left">{totalRows}</p>
               </div>
-              <div className="w-full lg:w-1/3 p-4">
+              <div className="w-full p-4">
                 <h3 className="text-xl font-bold text-gray-800 mb-3 text-left">
                   Department
                 </h3>
-                <p className="text-l text-gray-600 text-left">
-                  {roleData.dept}
-                </p>
+                <p className="text-l text-gray-600 text-left">{roleDept}</p>
               </div>
-              <div className="w-full lg:w-1/3 p-4">
+              <div className="w-full p-4">
                 <h3 className="text-xl font-bold text-gray-800 mb-3 text-left">
                   Application Close Date
                 </h3>
@@ -133,186 +137,138 @@ const ApplicantsListPage = () => {
             </div>
           </section>
 
-					<div
-						style={{
-							display: 'flex',
-							justifyContent: 'flex-start',
-							marginBottom: '-1px',
-						}}
-					>
-						<button
-							className={`hover:bg-gray-500 ${
-								view === 'Applied'
-									? 'bg-emerald-900'
-									: 'bg-gray-300'
-							} text-white py-2 px-6 rounded-md text-lg font-semibold`}
-							style={{
-								borderBottomRightRadius: '0',
-								borderBottomLeftRadius: '0',
-								marginLeft: '1px',
-							}}
-							onClick={selectApplicants}
-						>
-							Applicants
-						</button>
-						<button
-							className={`hover:bg-gray-500 ${
-								view === 'Shortlisted'
-									? 'bg-emerald-900'
-									: 'bg-gray-300'
-							} text-white py-2 px-6 rounded-md text-lg font-semibold`}
-							style={{
-								borderBottomRightRadius: '0',
-								borderBottomLeftRadius: '0',
-							}}
-							onClick={selectShortlist}
-						>
-							Shortlisted
-						</button>
-					</div>
-					<div className="overflow-x-auto">
-						<div
-							className="overflow-hidden border rounded"
-							style={{
-								borderRadius: '10px',
-								border: '1px solid #ccc',
-								borderTopLeftRadius: '0',
-							}}
-						>
-							<table
-								className="min-w-full border border-collapse"
-								style={{
-									borderRadius: '10px',
-									borderTopLeftRadius: '0',
-									overflow: 'hidden',
-									border: 'green',
-									borderTop: 'none',
-								}}
-							>
-								<thead className="border-b">
-									<tr className="text-white bg-emerald-900">
-										<th className="p-3">Staff Name</th>
-										<th className="p-3">Staff ID</th>
-										<th className="p-3">Email</th>
-										<th className="p-3">Current Role</th>
-										<th className="p-3">
-											Current Department
-										</th>
-										<th className="p-3">Location</th>
-										<th className="p-4">Skill Match (%)</th>
-									</tr>
-								</thead>
-								{
-									<tbody>
-										{(() => {
-											if (applicantsData.length === 0) {
-												return (
-													<tr className="border-b">
-														<td
-															colSpan={7}
-															className="p-4 text-center"
-														>
-															No Applicants Yet
-														</td>
-													</tr>
-												);
-											} else {
-												const rows = [];
-												for (
-													let i = 0;
-													i < totalRows;
-													i++
-												) {
-													const applicant =
-														applicantsData[i];
-													const staff_id =
-														applicant.staff
-															.staff_id;
-													const applicationId =
-														applicant.application_id;
-													if (
-														applicant.status ===
-														view
-													) {
-														rows.push(
-															<tr
-																key={i}
-																className="border-b hover:bg-gray-100"
-																onClick={() =>
-																	navigate(
-																		`/applicantdetail/${applicationId}`
-																	)
-																}
-															>
-																<td className="p-2">
-																	{
-																		applicant
-																			.staff
-																			.staff_name
-																	}
-																</td>
-																<td className="p-4">
-																	{staff_id}
-																</td>
-																<td className="p-4">
-																	{
-																		applicant
-																			.staff
-																			.email
-																	}
-																</td>
-																<td className="p-4">
-																	{
-																		applicant
-																			.staff
-																			.curr_role
-																	}
-																</td>
-																<td className="p-4">
-																	{
-																		applicant
-																			.staff
-																			.curr_dept
-																	}
-																</td>
-																<td className="p-4">
-																	{
-																		applicant
-																			.staff
-																			.location
-																	}
-																</td>
-																<td className="p-4">
-																	<ProgressBar
-																		percentage={parseInt(
-																			applicant.percentage_match
-																		)}
-																	/>
-																</td>
-															</tr>
-														);
-													}
-												}
-												if (
-													!roleData &&
-													!applicantsData
-												) {
-													return (
-														<div>Loading...</div>
-													);
-												}
-												return rows;
-											}
-										})()}
-									</tbody>
-								}
-							</table>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-start",
+              marginBottom: "-1px",
+            }}
+          >
+            <button
+              className={`hover:bg-gray-500 ${
+                view === "Applied" ? "bg-emerald-900" : "bg-gray-300"
+              } text-white py-2 px-6 rounded-md text-lg font-semibold`}
+              style={{
+                borderBottomRightRadius: "0",
+                borderBottomLeftRadius: "0",
+                marginLeft: "1px",
+              }}
+              onClick={selectApplicants}
+            >
+              Applicants
+            </button>
+            <button
+              className={`hover:bg-gray-500 ${
+                view === "Shortlisted" ? "bg-emerald-900" : "bg-gray-300"
+              } text-white py-2 px-6 rounded-md text-lg font-semibold`}
+              style={{
+                borderBottomRightRadius: "0",
+                borderBottomLeftRadius: "0",
+              }}
+              onClick={selectShortlist}
+            >
+              Shortlisted
+            </button>
+          </div>
+          <div className="overflow-x-auto">
+            <div
+              className="overflow-hidden border rounded"
+              style={{
+                borderRadius: "10px",
+                border: "1px solid #ccc",
+                borderTopLeftRadius: "0",
+              }}
+            >
+              <table
+                className="min-w-full border border-collapse"
+                style={{
+                  borderRadius: "10px",
+                  borderTopLeftRadius: "0",
+                  overflow: "hidden",
+                  border: "green",
+                  borderTop: "none",
+                }}
+              >
+                <thead className="border-b">
+                  <tr className="text-white bg-emerald-900">
+                    <th className="p-3">Staff Name</th>
+                    <th className="p-3">Staff ID</th>
+                    <th className="p-3">Email</th>
+                    <th className="p-3">Current Department</th>
+                    <th className="p-3">Location</th>
+                    <th className="p-4">Skill Match (%)</th>
+                  </tr>
+                </thead>
+                {
+                  <tbody>
+                    {(() => {
+                      if (totalRows == 0) {
+                        return (
+                          <tr className="border-b">
+                            <td colSpan={7} className="p-4 text-center">
+                              No Applicants Yet
+                            </td>
+                          </tr>
+                        );
+                      } else {
+                        const rows = [];
+                        for (let i = 0; i < totalRows; i++) {
+                          const applicant = applicantsData[i];
+                          const staff = applicant.staff;
+                          const applicationId = applicant.application_id;
+                          if (applicant.application_status === view) {
+                            /*fetch(
+                              `api/staff_role_skill?staff_id=${staff.staff_id}&role_id=${listing_id}`
+                            );still dont know le*/
+
+                            rows.push(
+                              <tr
+                                key={i}
+                                className="border-b hover:bg-gray-100"
+                                onClick={() =>
+                                  navigate(`/applicantdetail/${applicationId}`)
+                                }
+                              >
+                                <td className="p-2">
+                                  {staff.staff_fname} {staff.staff_lname}
+                                </td>
+                                <td className="p-4">{staff.staff_id}</td>
+                                <td className="p-4">{staff.email}</td>
+                                <td className="p-4">{staff.dept}</td>
+                                <td className="p-4">{staff.country}</td>
+                                <td className="p-4">
+                                  <ProgressBar
+                                    percentage={applicant.match_percentage}
+                                  />
+                                </td>
+                              </tr>
+                            );
+                          }
+                        }
+                        if (!listingData && !applicantsData) {
+                          return <div>Loading...</div>;
+                        }
+                        if (rows.length == 0) {
+                          return (
+                            <tr className="border-b">
+                              <td colSpan={7} className="p-4 text-center">
+                                No {view} Applicants Yet
+                              </td>
+                            </tr>
+                          );
+                        }
+                        return rows;
+                      }
+                    })()}
+                  </tbody>
+                }
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ApplicantsListPage;
