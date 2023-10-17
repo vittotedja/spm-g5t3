44a17dedfd1ea3015e61704255c3f8e43a22adfa
VITTO SURYA TEDJA <vittosuryat.2021@scis.smu.edu.sg>
2023-10-17 17:02:03 +0800

    test env

--- src/utilities/supabase.tsx
+++ src/utilities/supabase.tsx
@@ -1,8 +1,6 @@
 import {createClient, SupabaseClient} from '@supabase/supabase-js';
 
-const supabaseUrl: string =
-	import.meta.env.VITE_NEWEST_URL || process.env.VITE_NEWEST_URL || '';
-const supabaseKey: string =
-	import.meta.env.VITE_NEWEST_KEY || process.env.VITE_NEWEST_KEY || '';
+const supabaseUrl: string = import.meta.env.VITE_NEWEST_URL || '';
+const supabaseKey: string = import.meta.env.VITE_NEWEST_KEY || '';
 const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
 export default supabase;
