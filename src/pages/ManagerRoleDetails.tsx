f1c70f8c5d593251d9ac97a77c0be0eff51fb9e1
VITTO SURYA TEDJA <vittosuryat.2021@scis.smu.edu.sg>
2023-10-17 17:26:24 +0800

    testing another thing

--- src/pages/ManagerRoleDetails.tsx
+++ src/pages/ManagerRoleDetails.tsx
@@ -17,6 +17,8 @@ function ManagerRoleDetails() {
 				<div className="flex flex-row items-center justify-between px-3 pt-5">
 					<h1 className="text-3xl font-bold text-start">
 						All Posted Role Listings
+						{process.env.NODE_ENV === 'development' &&
+							'development'}
 					</h1>
 					{isHR && (
 						<Button
