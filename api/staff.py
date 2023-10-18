c4a2248db8d1e3c027df3e05d7df425dc6e28920
Jordian Renaldi <jrenaldi.2021@scis.smu.edu.sg>
2023-10-18 11:01:12 +0800

    fixed staff list endpoint for manager, added fucntionality for a listing

--- api/staff.py
+++ api/staff.py
@@ -60,42 +60,52 @@ async def staff(
         sorted_data = sorted_df.to_dict(orient="records")[0:5]
         return sorted_data
     elif isManager and staff_id and listing_id:
+        # 1. Fetch the staff table.
+        staff_data = supabase.from_("staff").select("*").execute().data
+        staff_df = pd.DataFrame(staff_data)
+
+        # 4. Filter out the staff who has applied to this listing.
+        applied_staff = supabase.from_("application").select("staff_id").eq("listing_id", listing_id).execute().data
+        applied_staff_ids = [item['staff_id'] for item in applied_staff]
+        merged_df = staff_df[~staff_df["staff_id"].isin(applied_staff_ids)]
+
+        # 5. Filter out the staff who is logged in (staff_id).
+        merged_df = merged_df[merged_df["staff_id"] != staff_id]
+
+        # 6. Make sure the staff has access control in [2,3,4].
+        merged_df = merged_df[merged_df["control_access"].isin([2, 3, 4])]
+
+        # 7. Fetch listing role skill set.
         listing_data = supabase.from_("listing").select("*, role(*)").eq("listing_id", listing_id).execute().data
         role_id = listing_data[0]["role"]["role_id"]
         role_skill = supabase.from_("role_skill").select("skill_id").eq("role_id", role_id).execute().data
         role_skill_set = {item['skill_id'] for item in role_skill}
-        staff = supabase.from_("staff").select("*").execute().data
-        staff_df = pd.DataFrame(staff)
-        skill_df = pd.DataFrame(supabase.from_("staff_skill").select("*").execute().data)
-        staff_skill_df = pd.merge(staff_df, skill_df, how="left", left_on="staff_id", right_on="staff_id")
-        skill_df = pd.DataFrame(supabase.from_("skill").select("*").execute().data)
-        df_merged_filtered = pd.merge(
-            staff_skill_df,
-            skill_df,
-            left_on="skill_id",
-            right_on="skill_id",
-        )
-        return (df_merged_filtered.iloc[0])
-        grouped_staff_skill = staff_skill_df.groupby('staff_id').agg({'skill_id': list}).reset_index()
-        merged_df = pd.merge(staff_df, grouped_staff_skill, on='staff_id', how='left')
-        staff_skills = supabase.from_("staff_skill").select("skill_id").eq("staff_id", staff_id).execute().data
-        staff_skill_set = {int(item['skill_id']) for item in staff_skills}
 
+        all_staff_skills = supabase.from_("staff_skill").select("*").execute().data
+        all_staff_skills_df = pd.DataFrame(all_staff_skills)
+
+        # Group by staff_id to get a list of skill_ids for each staff
+        merged_df_with_skills = pd.merge(merged_df, all_staff_skills_df, on="staff_id", how='inner')
+        # Assuming merged_df_with_skills is the result of your merge
+        grouped = merged_df_with_skills.groupby([
+            "staff_id", "staff_fname", "staff_lname", "dept", "country", "email", "control_access"
+        ])["skill_id"].apply(list).reset_index()
+
+        # 8. Calculate the match percentage for every staff.
         def calculate_match_percentage(skills):
             if isinstance(skills, list):
                 skill_set = {int(skill) for skill in skills if not math.isnan(skill)}
-                intersecting_skills = staff_skill_set.intersection(skill_set)
-                return (len(intersecting_skills) / len(skill_set)) * 100 if skill_set else 0
+                intersecting_skills = skill_set.intersection(role_skill_set)
+                return (len(intersecting_skills) / len(role_skill_set)) * 100 if skill_set else 0
             return 0
 
-        merged_df['match_percentage'] = merged_df['skill_id'].apply(calculate_match_percentage)
-        applied_staff = supabase.from_("application").select("staff_id").eq("listing_id", listing_id).execute().data
-        applied_staff_df = pd.DataFrame(applied_staff)
-        staff_who_havent_applied_df = merged_df[~merged_df["staff_id"].isin(applied_staff_df["staff_id"])]
-        staff_who_havent_applied_df = staff_who_havent_applied_df[staff_who_havent_applied_df["staff_id"] != staff_id]
-        staff_who_havent_applied_df = staff_who_havent_applied_df[staff_who_havent_applied_df["control_access"].isin([2, 3, 4])]
-        staff_who_havent_applied_df.sort_values(by=["match_percentage"], inplace=True)
-        return staff_who_havent_applied_df.to_dict(orient="records")
+        # Assuming staff_skill_set is the set of skills for the logged-in staff member
+        grouped['match_percentage'] = grouped['skill_id'].apply(calculate_match_percentage)
+
+        # 9. Sort by match percentage.
+        grouped.sort_values(by=["match_percentage"], ascending=False, inplace=True)
+
+        return grouped.to_dict(orient="records")
     elif staff_id:
         if staff_id == 0:
             staff = supabase.from_("staff").select("*").execute().data
