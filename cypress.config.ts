ea5e7c92e1d512671baac9947fda4a418230edb1
VITTO SURYA TEDJA <vittosuryat.2021@scis.smu.edu.sg>
2023-10-26 13:47:40 +0800

    add 45s buffer time

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,6 @@ import {defineConfig} from 'cypress';
 export default defineConfig({
 	e2e: {
 		baseUrl: 'http://localhost:5173',
-		defaultCommandTimeout: 15000,
+		defaultCommandTimeout: 45000,
 	},
 });
