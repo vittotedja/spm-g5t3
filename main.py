67860d0cd7959b03e4008fed556334cefec4940f
DennisH18 <111347933+DennisH18@users.noreply.github.com>
2023-10-27 11:27:58 +0800

    initial notification

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from api import (
     manager_rolelisting,
     listing,
     listing_manager,
+    notification
 )
 
 app = FastAPI()
@@ -21,6 +22,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 app.include_router(staff.router)
 app.include_router(staff_role.router)
 app.include_router(staff_skill.router)
@@ -31,3 +33,4 @@ app.include_router(application.router)
 app.include_router(listing.router)
 app.include_router(listing_manager.router)
 app.include_router(manager_rolelisting.router)
+app.include_router(notification.router)
