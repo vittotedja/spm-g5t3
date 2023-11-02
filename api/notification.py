8a5e945b67db260892dee3368f422df783d8afc6
DennisH18 <111347933+DennisH18@users.noreply.github.com>
2023-11-02 10:11:15 +0800

    edited notification, pending on how to call

--- api/notification.py
+++ api/notification.py
@@ -1,7 +1,5 @@
 from typing import List
-from fastapi_mail import ConnectionConfig, FastMail, MessageSchema, MessageType
-from pydantic import BaseModel, EmailStr
-from starlette.requests import Request
+from fastapi_mail import ConnectionConfig, FastMail, MessageSchema
 from starlette.responses import JSONResponse
 from fastapi import FastAPI, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
@@ -29,10 +27,8 @@ app.add_middleware(
 router = APIRouter()
 
 class ApplicationStatus(str, Enum):
-    Applied = "Applied"
     Shortlisted = "Shortlisted"
     Rejected = "Rejected"
-    Withdrawn = "Withdrawn"
 
 conf = ConnectionConfig(
    MAIL_USERNAME=os.getenv('MAIL_USERNAME'),
@@ -42,34 +38,28 @@ conf = ConnectionConfig(
    MAIL_STARTTLS=True,
    MAIL_SSL_TLS=False,
    MAIL_FROM=os.getenv('MAIL_USERNAME')
-   
 )
 
 @app.post("/api/notification")
 @router.post("/api/notification")
-async def send_email(status: str = None, application_id: int = None, listing_id: int = None):
+async def send_email(status: str = None, application_id: int = None, listing_id: int = None, staff_id: int = None):
 
-
-    application = (
+    if status == "Shortlisted":
+        application = (
             supabase.table("application")
             .select("*")
             .eq("application_id", application_id)
             .execute()
             .data
         )
-    
-    role = (
-        supabase.table("listing").select("*", "role(*)").eq("listing_id", application[0]["listing_id"]).execute().data
-    )
-    staff = (
-           supabase.table("staff").select("*").eq("staff_id", application[0]["staff_id"]).execute().data
-    )
-    
-    email = staff[0]["email"]
+        role = (
+            supabase.table("listing").select("*", "role(*)").eq("listing_id", application[0]["listing_id"]).execute().data
+        )
+        staff = (
+            supabase.table("staff").select("*").eq("staff_id", application[0]["staff_id"]).execute().data
+        )
+        email = staff[0]["email"]
 
-
-
-    if status == "Shortlisted":
         message = MessageSchema(
         subject="Application for "+ role[0]["role"]["role_name"] +" Shorlisted",
         recipients=[email],
@@ -102,6 +92,20 @@ async def send_email(status: str = None, application_id: int = None, listing_id:
         await fm.send_message(message)
         print(message)
     elif status == "Rejected":
+        application = (
+            supabase.table("application")
+            .select("*")
+            .eq("application_id", application_id)
+            .execute()
+            .data
+        )
+        role = (
+            supabase.table("listing").select("*", "role(*)").eq("listing_id", application[0]["listing_id"]).execute().data
+        )
+        staff = (
+            supabase.table("staff").select("*").eq("staff_id", application[0]["staff_id"]).execute().data
+        )
+        email = staff[0]["email"]
         message = MessageSchema(
         subject="Application for "+ role[0]["role"]["role_name"] +" Rejected",
         recipients=[email],
@@ -133,42 +137,102 @@ async def send_email(status: str = None, application_id: int = None, listing_id:
         fm = FastMail(conf)
         await fm.send_message(message)
         print(message)
-    elif listing_id:
-        staffs = supabase.table("application").select("staff_id").eq("listing_id", listing_id).execute().data[0]
-        emails = supabase.table("staff").select("email").eq("staff_id", staffs["staff_id"]).execute().data[0]
-
-        for email in emails.values():
+    elif status == "Invite" and listing_id and staff_id:
+        # Fetch role details
+        role_response = supabase.table("listing").select("*", "role(*)").eq("listing_id", listing_id).execute()
+        role = role_response.data if role_response.data else []
+        if not role:
+            return JSONResponse(status_code=400, content={"message": "Listing not found"})
+        # Fetch staff details
+        staff_response = supabase.table("staff").select("*").eq("staff_id", staff_id).execute()
+        staff_data = staff_response.data if staff_response.data else []
+        if not staff_data:
+            return JSONResponse(status_code=400, content={"message": "Staff not found"})
+        email_per_staff = staff_data[0].get("email")
+        if email_per_staff:
+            # Construct the invitation to apply email
             message = MessageSchema(
-            subject="Listing for "+ role[0]["role"]["role_name"] +" Updated",
-            recipients=[email],
-            body="""
-                <html>
-                <body style="font-family: Arial, sans-serif; margin: 0; padding: 0; background-color: #f4f4f4;">
-                    <div style="max-width: 600px; margin: 0 auto; background-color: #ffffff; padding: 20px; border-radius: 10px; box-shadow: 0 0 10px rgba(0,0,0,0.1); margin-top: 30px;">
-                        <div style="background-color: #299B71; color: #ffffff; text-align: center; padding: 10px; border-radius: 10px 10px 0 0;">
+                subject=f"Invitation to Apply for {role[0]['role']['role_name']} Position",
+                recipients=[email_per_staff],
+                body=f"""
+                    <html>
+                    <body style="font-family: Arial, sans-serif; margin: 0; padding: 0; background-color: #f4f4f4;">
+                        <div style="max-width: 600px; margin: 0 auto; background-color: #ffffff; padding: 20px; border-radius: 10px; box-shadow: 0 0 10px rgba(0,0,0,0.1); margin-top: 30px;">
+                            <div style="background-color: #299B71; color: #ffffff; text-align: center; padding: 10px; border-radius: 10px 10px 0 0;">
                                 <img src="https://ujjnudccckrqqtttlkoo.supabase.co/storage/v1/object/public/spm-assets/glasswindow_white.png" alt="Your Image Description" style="max-width: 15%; margin-bottom: 10px;">
-                            <h1>Glass Window</h1>
+                                <h1>Glass Window</h1>
+                            </div>
+                            <h1 style="color: #333333;">You're Invited!</h1>
+                            <p style="color: #555555;">Dear {staff_data[0]['staff_fname']} {staff_data[0]['staff_lname']},</p> 
+                            <p style="color: #555555;">We have been following your career with great interest and are impressed by your achievements. We believe that your skills and background align well with the {role[0]['role']['role_name']} role we have open.</p>
+                            <p style="color: #555555;">We would like to formally invite you to apply for this position at Glass Window. This role offers the opportunity to [include exciting opportunities, project details, impact of the role, etc.].</p>
+                            <p style="color: #555555;">Please visit our careers page or click the link below to submit your application and learn more about this exciting opportunity.</p>
+                            <div style="text-align: center; margin-top: 20px;">
+                                <a href="https://glasswindow.vercel.app/careers/{role[0]['role']['role_id']}" style="display: inline-block; background-color: #299B71; color: #ffffff; padding: 10px 20px; text-decoration: none; border-radius: 5px; transition: background-color 0.3s;">
+                                    Apply Now
+                                </a>
+                            </div>
+                            <p style="color: #555555;">We are looking forward to the possibility of you joining our team!</p>
+                            <p style="color: #555555;">Best regards,</p>
+                            <p style="color: #555555;">The Recruiting Team at Glass Window</p>
                         </div>
-                        <h1 style="color: #333333;">Listing Application Details</h1>
-                        <p style="color: #555555;">Dear """ + staff[0]["staff_fname"] + " " + staff[0]["staff_lname"]+ """,</p> 
-                        <p style="color: #555555;">We inform you that the Listing details for """ + role[0]["role"]["role_name"]+ """ has been Updated.</p>
-                        <p style="color: #555555;">Your Application is still submitted and will be considered.</p>
-                        <p style="color: #555555;">If you have any questions or would like feedback on your application, please feel free to reach out.</p>
-                        <div style="text-align: center; margin-top: 20px;">
-                            <a href="https://glasswindow.vercel.app" style="display: inline-block; background-color: #299B71; color: #ffffff; padding: 10px 20px; text-decoration: none; border-radius: 5px; transition: background-color 0.3s;">
-                                Go to Site
-                            </a>
-                        </div>
-                        <p style="color: #555555;">Best regards,</p>
-                        <p style="color: #555555;">All in One Hiring Team</p>
-                    </div>
-                </body>
-                </html>
-                """,           
-            subtype="html") 
+                    </body>
+                    </html>
+                    """,
+                subtype="html"
+            )
             fm = FastMail(conf)
             await fm.send_message(message)
             print(message)
+    elif listing_id:
+        role = (
+            supabase.table("listing").select("*", "role(*)").eq("listing_id", listing_id).execute().data
+        )
+        response = supabase.table("application").select("staff_id").eq("listing_id", listing_id).execute()
+        staffs = response.data if response.data else []
+        for staff_dict in staffs:
+            staff_id = staff_dict.get("staff_id")
+            if staff_id is None:
+                continue  # skip this iteration if staff_id is None
+            staff_response = supabase.table("staff").select("*").eq("staff_id", staff_id).execute()
+            staff_data = staff_response.data if staff_response.data else []
+            if not staff_data:
+                continue  # skip this iteration if no data found
+            email_per_staff = staff_data[0].get("email")
+            if email_per_staff:
+                # ... create your message and send email
+                # ensure you are handling the rest correctly
+                message = MessageSchema(
+                subject="Listing for "+ role[0]["role"]["role_name"] +" Updated",
+                recipients=[email_per_staff],
+                body="""
+                    <html>
+                    <body style="font-family: Arial, sans-serif; margin: 0; padding: 0; background-color: #f4f4f4;">
+                        <div style="max-width: 600px; margin: 0 auto; background-color: #ffffff; padding: 20px; border-radius: 10px; box-shadow: 0 0 10px rgba(0,0,0,0.1); margin-top: 30px;">
+                            <div style="background-color: #299B71; color: #ffffff; text-align: center; padding: 10px; border-radius: 10px 10px 0 0;">
+                                    <img src="https://ujjnudccckrqqtttlkoo.supabase.co/storage/v1/object/public/spm-assets/glasswindow_white.png" alt="Your Image Description" style="max-width: 15%; margin-bottom: 10px;">
+                                <h1>Glass Window</h1>
+                            </div>
+                            <h1 style="color: #333333;">Listing Application Details</h1>
+                            <p style="color: #555555;">Dear """ + staff_data[0]["staff_fname"] + " " + staff_data[0]["staff_lname"]+ """,</p> 
+                            <p style="color: #555555;">We inform you that the Listing details for """ + role[0]["role"]["role_name"]+ """ has been Updated.</p>
+                            <p style="color: #555555;">Your Application is still submitted and will be considered.</p>
+                            <p style="color: #555555;">If you have any questions or would like feedback on your application, please feel free to reach out.</p>
+                            <div style="text-align: center; margin-top: 20px;">
+                                <a href="https://glasswindow.vercel.app" style="display: inline-block; background-color: #299B71; color: #ffffff; padding: 10px 20px; text-decoration: none; border-radius: 5px; transition: background-color 0.3s;">
+                                    Go to Site
+                                </a>
+                            </div>
+                            <p style="color: #555555;">Best regards,</p>
+                            <p style="color: #555555;">All in One Hiring Team</p>
+                        </div>
+                    </body>
+                    </html>
+                    """,           
+                subtype="html") 
+                fm = FastMail(conf)
+                await fm.send_message(message)
+                print(message)
 
     return JSONResponse(status_code=200, content={"message": "email has been sent"})
     
